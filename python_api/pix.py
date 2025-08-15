c4e8a66f978061be07367bab587bbbca83a78f53
Agencia Kivora <kivoraofc@gmail.com>
2025-08-15 10:54:34 -0300

    Update pix.py

--- python_api/pix.py
+++ python_api/pix.py
@@ -77,8 +77,10 @@ def index():
 def static_files(filename):
     return send_from_directory(FRONTEND_DIR, filename)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
 
 #Porta dinâmica no final do pix.py:
 #Comando de start no Railway: gunicorn pix:app
